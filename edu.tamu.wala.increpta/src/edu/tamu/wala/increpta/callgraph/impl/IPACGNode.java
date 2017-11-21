704cfbc72f4c44cc50edf62eb23454a5b79f6048
Bozhen Liu <april1989@tamu.edu>
2017-11-20 23:55:16 -0600

    add Eclipse Public License v1.0

--- edu.tamu.wala.increpta/src/edu/tamu/wala/increpta/callgraph/impl/IPACGNode.java
+++ edu.tamu.wala.increpta/src/edu/tamu/wala/increpta/callgraph/impl/IPACGNode.java
@@ -1,17 +1,17 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Bozhen Liu, Jeff Huang.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Bozhen Liu, Jeff Huang - initial API and implementation
+ ******************************************************************************/
 package edu.tamu.wala.increpta.callgraph.impl;
 
-import java.util.Iterator;
-
 import com.ibm.wala.classLoader.CallSiteReference;
-import com.ibm.wala.classLoader.IMethod;
-import com.ibm.wala.classLoader.NewSiteReference;
 import com.ibm.wala.ipa.callgraph.CGNode;
-import com.ibm.wala.ipa.callgraph.Context;
-import com.ibm.wala.ipa.callgraph.ContextItem;
-import com.ibm.wala.ipa.cha.IClassHierarchyDweller;
-import com.ibm.wala.ssa.DefUse;
-import com.ibm.wala.ssa.IR;
-import com.ibm.wala.util.graph.INodeWithNumber;
 
 
 public interface IPACGNode extends CGNode {
--- edu.tamu.wala.increpta/src/edu/tamu/wala/increpta/ipa/callgraph/propagation/IPAReflectionHandler.java
+++ edu.tamu.wala.increpta/src/edu/tamu/wala/increpta/ipa/callgraph/propagation/IPAReflectionHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2017 Bozhen Liu, Jeff Huang.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Bozhen Liu, Jeff Huang - initial API and implementation
+ ******************************************************************************/
 package edu.tamu.wala.increpta.ipa.callgraph.propagation;
 
 import java.util.Collection;
